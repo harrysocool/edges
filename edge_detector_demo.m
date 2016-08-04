afc224665d992a83ba3d22aba7d821d38aa9d5fe
harrysocool <harry_so_cool@live.com>
2016-08-04 10:33:52 +0100

    pass the alpha&Beta value

--- edge_detector_demo.m
+++ edge_detector_demo.m
@@ -1,4 +1,4 @@
-function boxes = edge_detector_demo(image_filepath)
+function boxes = edge_detector_demo(image_filepath, model_name, alpha, beta)
     addpath('private');
     addpath('models');
     addpath('toolbox');
@@ -20,13 +20,13 @@ function boxes = edge_detector_demo(image_filepath)
     end
 
     %% load pre-trained edge detection model and set opts (see edgesDemo.m)
-    model=load('models/forest/modelEAR0.4_2.mat'); model=model.model;
+    model=load(['models/forest/model' model_name '.mat']); model=model.model;
     model.opts.multiscale=1; model.opts.sharpen=2; model.opts.nThreads=4;
 
     %% set up opts for edgeBoxes (see edgeBoxes.m)
     opts = edgeBoxes;
-    opts.alpha = .25;     % step size of sliding window search
-    opts.beta  = .75;     % nms threshold for object proposals
+    opts.alpha = alpha;     % step size of sliding window search
+    opts.beta  = beta;     % nms threshold for object proposals
     opts.minScore = .01;  % min score of boxes to detect
     opts.maxBoxes = 1e4;  % max number of boxes to detect
 
